b5e7d1054d67a9fa73694292412763fb57ada08c
NathanCHEFF <54266398+NathanCHEFF@users.noreply.github.com>
2021-12-17 09:26:48 +0200

    Update flashWrite.sh

--- flashWrite.sh
+++ flashWrite.sh
@@ -33,18 +33,99 @@
 VFILE=/home/smart/Downloads/libzavod-sd-image-master-1845.img
 echo 'Запуск программы записи флешки'
 echo ''
+echo 'https://github.com/NathanCHEFF/flashWriter-4-NiK-Electronics-/'
+echo 'У меня работало без форматирования и отмонтирования устройства.'
+echo 'Просто вводи "n"'
 echo ''
 echo ''
-echo 'путь к файлу образа '$VFILE
 echo ''
-echo 'Размонтируем'
-#sudo umount /dev/sdb
+echo 'путь к файлу образа '$VFILE' ?'
+read RESP
+if [ "$RESP" != "y" ] && [ "$RESP" != "Y" ] && [ "$RESP" != "д" ] && [ "$RESP" != "Д" ] ; then
+    echo "введите путь к файлу:"
+	read VFILE
+
+fi
+
+if [! -f $VFILE ] ; then
+	echo "неправильная директория или нет файла"
+	exit
+fi
+
 echo 'Форматировать флешку?(Y/n)?'
-read NAME
-echo $NAME
-#sudo mkfs -t ext4 -L FLASH /dev/sdb 
-echo $VFILE
+read RESP
+if [ "$RESP" != "y" ] && [ "$RESP" != "Y" ] && [ "$RESP" != "д" ] && [ "$RESP" != "Д" ] ; then
+	echo  "форматирование отменено!"
+else
+	echo 'Размонтируем'
+	#sudo umount /dev/sdb
+	#sudo mkfs -t ext4 -L FLASH /dev/sdb
+fi
 #sudo dd if=/home/smart/Downloads/libzavod-sd-image-master-1845.img of=/dev/sdb status=progress
-#sudo dd if=/home/smart/Downloads/libzavod-sd-image-master-1845.img of=/dev/sdb
+sudo dd if=$VFILE of=/dev/sdb  
+#sudo dd if=$VFILE of=/dev/sdb  | echo '\r\n' sudo kill -USR1 $(pgrep ^dd)
 #sudo dd if=/home/smart/Downloads/libzavod-sd-image-master-1845.img of=/dev/sdb | watch -n1 'sudo kill -USR1 $(pgrep ^dd)'
+clear
 sudo sync
+echo 'screen clear'
+echo "Синхронизация прошла с кодом "$RANDOM
+echo 'Создание файла interfaces'
+##lsblk
+#blkid -s UUID -o value /dev/sdb1
+#
+#
+#
+#
+UUID=$(blkid -s UUID -o value /dev/sdb1)
+VFILE=/media/smart/"$UUID"/interface
+
+if [ ! -f $VFILE ] ; then
+	echo  "файла нет!"
+	sudo cat > $VFILE
+fi
+if [ ! -f $VFILE ] ; then
+	echo  "ошибка создания файла"
+	exit 0
+fi
+
+#
+# 10.3.33.[60~92] (E32)
+#	60+RESP
+#
+#
+
+echo "Ведите номер места(или КС)"
+read RESP
+IPADR=$(( RESP + 59 ))
+MACADR=$(echo "obase=16; "$(( RESP + 212 )) | bc)
+echo "Ip адрес устройства  10.3.33."$IPADR
+echo "Mac адрес устройства b6:d0:5e:0f:"$MACADR":16"
+echo ""
+echo "Запись данных"
+
+echo '# INTERFACES ###############################'"\r" > $VFILE
+echo '# USE_BRANCH master ########################'"\r" >> $VFILE
+echo ''"\r" >> $VFILE
+echo '# '$RESP' mountplace'"\r" >> $VFILE
+echo ''"\r" >> $VFILE
+echo 'auto lo'"\r" >> $VFILE
+echo 'iface lo inet loopback'"\r" >> $VFILE
+echo ''"\r" >> $VFILE
+echo '#auto eth1'"\r" >> $VFILE
+echo 'allow-hotplug eth1'"\r" >> $VFILE
+echo 'iface eth1 inet static'"\r" >> $VFILE
+echo "\t"'hwaddress ether 02:fb:85:37:a5:fa'"\r" >> $VFILE
+echo "\t"'address 10.2.33.240'"\r" >> $VFILE
+echo "\t"'netmask 255.255.255.0'"\r" >> $VFILE
+echo "\t"'metric 2'"\r" >> $VFILE
+echo ''"\r" >> $VFILE
+echo '#auto eth0'"\r" >> $VFILE
+echo 'allow-hotplug eth0'"\r" >> $VFILE
+echo 'iface eth0 inet static'"\r" >> $VFILE
+echo "\t"'hwaddress ether b6:d0:5e:0f:'$MACADR':16'"\r" >> $VFILE
+echo "\t"'address 10.3.33.'$IPADR"\r" >> $VFILE
+echo "\t"'netmask 255.255.255.0'"\r" >> $VFILE
+echo "\t"'gateway 10.3.33.250'"\r" >> $VFILE
+echo "\t"'metric 3' >> $VFILE
+echo ""
+echo "$(cat $VFILE)"
